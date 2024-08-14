79fdadaef2025ba8f77d93ebb069ee3936fc324a
Denis Iskakov <iskak123ov@gmail.com>
2024-08-14 13:02:21 +0400

    fix

--- sprint1/problems/async_server/solution/src/main.cpp
+++ sprint1/problems/async_server/solution/src/main.cpp
@@ -9,51 +9,71 @@
 #include "http_server.h"
 
 namespace {
-namespace net = boost::asio;
-using namespace std::literals;
-namespace sys = boost::system;
-namespace http = boost::beast::http;
+    namespace net = boost::asio;
+    using namespace std::literals;
+    namespace sys = boost::system;
+    namespace http = boost::beast::http;
 
-// Запрос, тело которого представлено в виде строки
-using StringRequest = http::request<http::string_body>;
-// Ответ, тело которого представлено в виде строки
-using StringResponse = http::response<http::string_body>;
+    // Запрос, тело которого представлено в виде строки
+    using StringRequest = http::request<http::string_body>;
+    // Ответ, тело которого представлено в виде строки
+    using StringResponse = http::response<http::string_body>;
 
-struct ContentType {
-    ContentType() = delete;
-    constexpr static std::string_view TEXT_HTML = "text/html"sv;
-    // При необходимости внутрь ContentType можно добавить и другие типы контента
-};
+    struct ContentType {
+        ContentType() = delete;
+        constexpr static std::string_view TEXT_HTML = "text/html"sv;
+        // При необходимости внутрь ContentType можно добавить и другие типы контента
+    };
 
-// Создаёт StringResponse с заданными параметрами
-StringResponse MakeStringResponse(http::status status, std::string_view body, unsigned http_version,
-                                  bool keep_alive,
-                                  std::string_view content_type = ContentType::TEXT_HTML) {
-    StringResponse response(status, http_version);
-    response.set(http::field::content_type, content_type);
-    response.body() = body;
-    response.content_length(body.size());
-    response.keep_alive(keep_alive);
-    return response;
-}
-
-StringResponse HandleRequest(StringRequest&& req) {
-    // Подставьте сюда код из синхронной версии HTTP-сервера
-    return MakeStringResponse(http::status::ok, "OK"sv, req.version(), req.keep_alive());
-}
-
-// Запускает функцию fn на n потоках, включая текущий
-template <typename Fn>
-void RunWorkers(unsigned n, const Fn& fn) {
-    n = std::max(1u, n);
-    std::vector<std::jthread> workers;
-    workers.reserve(n - 1);
-    // Запускаем n-1 рабочих потоков, выполняющих функцию fn
-    while (--n) {
-        workers.emplace_back(fn);
+    // Создаёт StringResponse с заданными параметрами
+    StringResponse MakeStringResponse(http::status status, std::string_view body, size_t size, unsigned http_version,
+        bool keep_alive,
+        std::string_view content_type = ContentType::TEXT_HTML) {
+        StringResponse response(status, http_version);
+        response.set(http::field::content_type, content_type);
+        if (status == http::status::method_not_allowed) {
+            response.set(http::field::allow, "GET, HEAD");
+        }
+        response.body() = body;
+        response.content_length(size);
+        response.keep_alive(keep_alive);
+        return response;
     }
-    fn();
-}
+
+    StringResponse HandleRequest(StringRequest&& req) {
+        const auto text_response = [&req](http::status status, std::string_view text, size_t size) {
+            return MakeStringResponse(status, text, size, req.version(), req.keep_alive());
+        };
+
+        std::string response_body("Hello, "sv);
+        auto req_method = req.method();
+        std::string_view req_target = req.target();
+        response_body += std::string(req_target.begin() + 1, req_target.end());
+
+        if (req_method == http::verb::get) {
+            return text_response(http::status::ok, response_body, response_body.size());
+        }
+        else if (req_method == http::verb::head) {
+            return text_response(http::status::ok, "", response_body.size());
+        }
+        else {
+            std::string response_not_allowed_body("Invalid method"sv);
+            return text_response(http::status::method_not_allowed, response_not_allowed_body, response_not_allowed_body.size());
+        }
+    }
+
+    // Запускает функцию fn на n потоках, включая текущий
+    template <typename Fn>
+    void RunWorkers(unsigned n, const Fn& fn) {
+        n = std::max(1u, n);
+        std::vector<std::jthread> workers;
+        workers.reserve(n - 1);
+        // Запускаем n-1 рабочих потоков, выполняющих функцию fn
+        while (--n) {
+            workers.emplace_back(fn);
+        }
+        fn();
+    }
 
 }  // namespace
 
@@ -68,18 +88,20 @@ int main() {
         if (!ec) {
             ioc.stop();
         }
-    });
+        std::cout << "Server has been finished..."sv << std::endl;
+        });
 
     const auto address = net::ip::make_address("0.0.0.0");
     constexpr net::ip::port_type port = 8080;
-    http_server::ServeHttp(ioc, {address, port}, [](auto&& req, auto&& sender) {
-        // sender(HandleRequest(std::forward<decltype(req)>(req)));
-    });
+    // Обрабатываем запрос. 
+    http_server::ServeHttp(ioc, { address, port }, [](auto&& req, auto&& sender) {
+        sender(HandleRequest(std::forward<decltype(req)>(req)));
+        });
 
     // Эта надпись сообщает тестам о том, что сервер запущен и готов обрабатывать запросы
     std::cout << "Server has started..."sv << std::endl;
 
     RunWorkers(num_threads, [&ioc] {
         ioc.run();
-    });
+        });
 }
