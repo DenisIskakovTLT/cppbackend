843d37ed0577b92c38ef0b3ff0c9ac3d1778eb99
Denis Iskakov <iskak123ov@gmail.com>
2024-08-29 23:49:17 +0400

    1 commit 2 sprint

---
+++ sprint2/problems/final_task/src/other/utils.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <random>
+#include <string_view>
+#include <vector>
+
+namespace requestHandler {
+
+	std::vector<std::string_view> SplitStr(std::string_view str);
+	std::string GetBearerToken(std::string_view bearer_string);
+
+}//namespace requestHandler
+
+namespace randomgen {
+
+	double RandomDouble(const double thl, const double thh);
+	int RandomInt(const int thl, const int thh);
+
+}//namespace random
